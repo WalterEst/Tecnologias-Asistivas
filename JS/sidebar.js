46d69040fffd9fa2d65148b4406e8517bef2cc24
Nain Burgos <burgosnain2@gmail.com>
2023-11-05 16:20:41 -0300

    agregando recargar y escala de grises

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -4,7 +4,9 @@ const menuItems = sidebar.querySelectorAll('a');
 const invertColorsButton = document.getElementById('invertColorsButton');
 const increaseFontSizeButton = document.getElementById('increaseFontSizeButton');
 const decreaseFontSizeButton = document.getElementById('decreaseFontSizeButton');
-const grayscaleButton = document.getElementById('grayscaleButton'); 
+const grayscaleButton = document.getElementById('grayscaleButton');
+const resetButton = document.getElementById('resetButton');
+
 toggleButton.addEventListener('click', () => {
     sidebar.classList.toggle('open');
 });
@@ -14,38 +16,30 @@ invertColorsButton.addEventListener('click', () => {
 });
 
 increaseFontSizeButton.addEventListener('click', () => {
-    // Obtener el tamaño actual del texto
     const currentSize = window.getComputedStyle(document.body, null).getPropertyValue('font-size');
-    
-    // Aumentar el tamaño del texto (puedes ajustar el valor según tus necesidades)
-    const newSize = parseFloat(currentSize) * 1.2; // Aumenta el tamaño en un 20%
-    
-    // Aplicar el nuevo tamaño del texto
+    const newSize = parseFloat(currentSize) * 1.2;
     document.body.style.fontSize = newSize + 'px';
 });
 
 decreaseFontSizeButton.addEventListener('click', () => {
-    // Obtener el tamaño actual del texto
     const currentSize = window.getComputedStyle(document.body, null).getPropertyValue('font-size');
-    
-    // Disminuir el tamaño del texto (puedes ajustar el valor según tus necesidades)
-    const newSize = parseFloat(currentSize) / 1.2; // Disminuye el tamaño en un 20%
-    
-    // Aplicar el nuevo tamaño del texto
+    const newSize = parseFloat(currentSize) / 1.2;
     document.body.style.fontSize = newSize + 'px';
 });
 
 grayscaleButton.addEventListener('click', () => {
-    // Verificar si la escala de grises ya está aplicada
     const isGrayscale = document.documentElement.style.filter === 'grayscale(100%)';
 
-    // Si la escala de grises está aplicada, quitarla; de lo contrario, aplicarla
     if (isGrayscale) {
         document.documentElement.style.filter = '';
     } else {
-        // Aplicar la escala de grises
         const grayscaleColors = ['#3c3c3c', '#515151', '#666666', '#8c8c8c', '#b5b5b5'];
         document.documentElement.style.filter = `progid:DXImageTransform.Microsoft.gradient(startColorstr=${grayscaleColors[0]}, endColorstr=${grayscaleColors[4]}, GradientType=0)`;
         document.documentElement.style.filter = `grayscale(100%)`;
     }
+});
+
+// Agrega el evento para resetear la página
+resetButton.addEventListener('click', () => {
+    location.reload(true); // Recarga la página forzando la recarga desde el servidor
 });
