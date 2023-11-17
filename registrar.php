4d9ea2f1cbd8f86d83e8f9d02d71a7bc63fb490d
Walter Vásquez <waltervasquez.2002@gmail.com>
2023-11-16 22:53:49 -0300

    Agregando ultimos cambios

--- registrar.php
+++ registrar.php
@@ -3,36 +3,35 @@
 include("db_co.php");
 
 // Verifica si se ha enviado el formulario
-if (isset($_POST['Enviar'])) {
-    // Comprueba si los campos 'name' y 'email' tienen al menos 1 caracter
-    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
+if (isset($_POST['register'])) {
+    // Comprueba si los campos 'nombre' y 'edad' tienen al menos 1 caracter
+    if (strlen($_POST['nombre']) >= 1 && strlen($_POST['edad']) >= 1) {
         // Se eliminan espacios en blanco al principio y al final de los datos
         $nombre = trim($_POST['nombre']);
-        $edad = trim($_POST['edadl']);
+        $edad = trim($_POST['edad']);
         $apellido_paterno = trim($_POST['apellido_paterno']);
         $apellido_materno = trim($_POST['apellido_materno']);
         // Se obtiene la fecha actual
         $fechareg = date("d/m/y");
         // Se crea la consulta SQL para insertar los datos en la base de datos
-        $consulta = "INSERT INTO datos_usuario($nombre, apellido_paterno, apellido_materno, edad, fecha) VALUES ('$nombre','$edad','$apellido_paterno','$apellido_materno','$fechareg')";
+        $consulta = "INSERT INTO datos_usuario(nombre, apellido_paterno, apellido_materno, edad, fecha) VALUES ('$nombre','$apellido_paterno','$apellido_materno','$edad','$fechareg')";
         // Se ejecuta la consulta en la base de datos
         $resultado = mysqli_query($conex, $consulta);
         // Verifica si la consulta se ejecutó correctamente
         if ($resultado) {
-            ?> 
-            <!-- Muestra un mensaje de éxito si la inserción fue exitosa -->
-            <h3 class="ok">¡Te has inscripto correctamente!</h3>
-            <?php
+            
+            header("Location: Atencion_y_concentracion/seccion-1-1.html");
+            exit(); // Es importante salir después de la redirección
         } else {
             ?> 
             <!-- Muestra un mensaje de error si la inserción falló -->
-            <h3 class="bad">¡Ups ha ocurrido un error!</h3>
+            <h3 class="bad">Error al ingresar</h3>
             <?php
         }
     } else {
         ?> 
         <!-- Muestra un mensaje si los campos no están completos -->
-        <h3 class="bad">¡Por favor complete los campos!</h3>
+        <h3 class="bad">¡Por favor complete todos los campos!</h3>
         <?php
     }
 }
