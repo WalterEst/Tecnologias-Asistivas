8ec19dcd7d43106ed5b23ac34876a941f98946a2
Walter Vásquez <waltervasquez.2002@gmail.com>
2023-11-25 13:40:19 -0300

    Agregando cambios en sidebar en general

---
+++ JS/lector.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const contentToRead = document.querySelector('.bloque-enunciado');
+    const readAloudButton = document.getElementById('readAloudButton');
+
+    readAloudButton.addEventListener('click', function (event) {
+        event.preventDefault(); // Previene el comportamiento predeterminado del enlace
+
+        const textToRead = contentToRead.innerText;
+
+        const speech = new SpeechSynthesisUtterance(textToRead);
+        speech.lang = 'es-LA';
+        speech.rate = 1.1;
+
+        speechSynthesis.speak(speech);
+    });
+});
